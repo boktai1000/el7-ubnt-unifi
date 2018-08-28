a34232290cc6d6d15989581037db74061e8649e4
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2018-08-27 20:54:17 -0500

    Update el7-update-unifi.sh

--- scripts/el7-update-unifi.sh
+++ scripts/el7-update-unifi.sh
@@ -2,7 +2,7 @@
 
 #Update UniFi:
 
-#You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/el7-ubnt-unifi/master/el7-update-unifi.sh | sudo bash
+#You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/el7-ubnt-unifi/master/scripts/el7-update-unifi.sh | sudo bash
 
 #Set version numbers for download:
 unifiversion="$(curl -L --silent https://help.ubnt.com/hc/en-us/articles/115000441548-UniFi-Current-Controller-Versions | grep -oP "LTS \K([0-9\.]+)")"
