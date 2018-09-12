9779d89132369de49199ac7a1fca202faba60dcc
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2018-09-12 13:18:08 -0500

    move around execution command

--- scripts/el7-install-unifi.sh
+++ scripts/el7-install-unifi.sh
@@ -4,13 +4,13 @@
 #Based on https://community.ubnt.com/t5/UniFi-Wireless/Installing-UniFi-on-CentOS7-as-a-service/m-p/1973439/highlight/true#M234790
 
 #Download the script with wget https://raw.githubusercontent.com/boktai1000/el7-ubnt-unifi/master/scripts/el7-install-unifi.sh
-#Set the script as executable with chmod +x el7-install-unifi.sh
 
 #REQUIRED / IMPORTANT OR ELSE THE SCRIPT WILL FAIL TO DOWNLOAD
 #Modify your script with vi el7-install-unifi.sh to set your version for the UniFi Controller
 #Select your version number from the current UniFi Controller software versions web page https://help.ubnt.com/hc/en-us/articles/360008240754#1
 #Replace unifiversion="x.x.xx" with the version number you want to target from the web page, leave the version number inside the quotes
 
+#Set the script as executable with chmod +x el7-install-unifi.sh
 #Run the script with sudo ./el7-install-unifi.sh
 
 #Set version numbers for download here:
