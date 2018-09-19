93ea0ee4ceaa8a2426b576f74264e703bb7b8b27
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2018-09-19 15:31:53 -0500

    Add three installation options

--- scripts/el7-install-unifi.sh
+++ scripts/el7-install-unifi.sh
@@ -2,19 +2,43 @@
 
 #Install UniFi:
 #Based on https://community.ubnt.com/t5/UniFi-Wireless/Installing-UniFi-on-CentOS7-as-a-service/m-p/1973439/highlight/true#M234790
-
-#Download the script with wget https://raw.githubusercontent.com/boktai1000/el7-ubnt-unifi/master/scripts/el7-install-unifi.sh
-
-#REQUIRED / IMPORTANT OR ELSE THE SCRIPT WILL FAIL TO DOWNLOAD
-#Modify your script with vi el7-install-unifi.sh to set your version for the UniFi Controller
-#Select your version number from the current UniFi Controller software versions web page https://help.ubnt.com/hc/en-us/articles/360008240754#1
-#Replace unifiversion="x.x.xx" with the version number you want to target from the web page, leave the version number inside the quotes
-
-#Set the script as executable with chmod +x el7-install-unifi.sh
-#Run the script with sudo ./el7-install-unifi.sh
-
-#Set version numbers for download here:
-unifiversion="x.x.xx"
+#
+###Option 1: (Default) Install LTS Stable 
+##You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/el7-ubnt-unifi/master/scripts/el7-install-unifi.sh | sudo bash
+##LTS Stable
+unifiversion="$(curl --silent --include --no-buffer --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" https://help.ubnt.com/hc/en-us/articles/360008240754#1 | grep -A1 ">LTS Stable<" | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
+#
+#
+#
+###Option 2: Grab latest version of other branches, be sure to comment out LTS Stable, and then uncomment your version of choice
+##Download the script with wget https://raw.githubusercontent.com/boktai1000/el7-ubnt-unifi/master/scripts/el7-install-unifi.sh
+##Modify your script with vi el7-install-unifi.sh to set your version for the UniFi Controller
+##Set the script as executable with chmod +x el7-install-unifi.sh
+##Run the script with sudo ./el7-install-unifi.sh
+##
+##LTS Stable Candidate
+#unifiversion="$(curl --silent --include --no-buffer --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" https://help.ubnt.com/hc/en-us/articles/360008240754#1 | grep -A1 ">LTS Stable Candidate<" | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
+#
+##Stable
+#unifiversion="$(curl --silent --include --no-buffer --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" https://help.ubnt.com/hc/en-us/articles/360008240754#1 | grep -A1 ">Stable<" | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
+#
+##Stable Candidate (Beta Blog)
+#unifiversion="$(curl --silent --include --no-buffer --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" https://help.ubnt.com/hc/en-us/articles/360008240754#1 | grep -A1 ">Stable Candidate (Beta Blog)<" | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
+#
+##Stable Candidate (Updates Blog)
+#unifiversion="$(curl --silent --include --no-buffer --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Sec-WebSocket-Version: 13" https://help.ubnt.com/hc/en-us/articles/360008240754#1 | grep -A1 ">Stable Candidate (Updates Blog)<" | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
+#
+#
+#
+###Option 3: Manually target version of choice, be sure to comment out LTS Stable and then uncomment unifiversion="x.x.xx"
+##Replace unifiversion="x.x.xx" with the version number you want to target from the web page, leave the version number inside the quotes
+##Set the script as executable with chmod +x el7-install-unifi.sh
+##Run the script with sudo ./el7-install-unifi.sh
+#unifiversion="x.x.xx"
+#
+#
+#
+###MongoDB Version 3.4 Recommended
 mongodbversion="3.4"
 
 #Create mongodb Repo file
